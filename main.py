4b4cc17d4ff00c8cf4dd595037abbfa84f3e6ed2
BATUHAN KIRICI <133860125+batuhankirici2@users.noreply.github.com>
2025-10-31 13:48:29 +0100

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T12:48:28.053Z
-Commit: Add export functionality for reports
+Timestamp: 2025-10-31T12:48:29.163Z
+Commit: Introduce real-time notifications
