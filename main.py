e1130ab0d6b135d01c4acefccfc40106d12fc85c
BATUHAN KIRICI <133860125+batuhankirici2@users.noreply.github.com>
2025-10-31 13:48:31 +0100

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T12:48:29.163Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-10-31T12:48:31.294Z
+Commit: Patch API endpoint error handling
