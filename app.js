ee1277c30781f13483d29ee0df728137b72aba41
BATUHAN KIRICI <133860125+batuhankirici2@users.noreply.github.com>
2025-10-31 13:48:33 +0100

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T12:48:24.574Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-10-31T12:48:32.579Z
+Commit: Fix CSS styling bug on mobile devices
